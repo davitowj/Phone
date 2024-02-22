17bbf33cd40a917421b91d00ef053fc16d4fdef3
davitowj <davitowinner@gmail.com>
2024-02-22 08:52:15 +0700

    first commit

---
+++ ContohInterface.java
@@ -0,0 +1,8 @@
+package com.mycompany.contohinterface;
+
+public class ContohInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Xiaomi.java
@@ -0,0 +1,66 @@
+package com.mycompany.contohinterface;
+public class Xiaomi implements Phone {
+    private int volume;
+    private boolean isPowerOn;
+
+    public Xiaomi() {
+        //volume awal
+        this.volume = 50;
+    }
+
+    @Override
+    public void powerOn() {
+        if (isPowerOn == false) {
+            isPowerOn = true;
+            System.out.println("Handphone menyala...");
+            System.out.println("Selamat datang di XIAOMI");
+            System.out.println("Android version 50");
+        } else {
+            System.out.println("Handphone sudah menyala");
+        }
+    }
+
+    @Override
+    public void powerOff() {
+        if (isPowerOn == true) {
+            isPowerOn = false;
+            System.out.println("Handphone dimatikan");
+        } else {
+            System.out.println("Handphone sudah dimatikan");
+        }
+    }
+
+    @Override
+    public void volumeUp() {
+        if (isPowerOn) {
+            if (this.volume == MAX_VOLUME) {
+                System.out.println("Volume FULL!!");
+                System.out.println("sudah " + this.getVolume() + "%");
+            } else {
+                this.volume += 10;
+                System.out.println("Volume sekarang: " + this.getVolume());
+            }
+        } else {
+            System.out.println("Nyalakan dulu donk HP-nya!!");
+        }
+    }
+    
+    @Override
+    public void volumeDown() {
+        if (isPowerOn) {
+            if (this.volume == MIN_VOLUME) {
+                System.out.println("Volume = 0%");
+            } else {
+                this.volume -= 10;
+                System.out.println("Volume sekarang: " + this.getVolume());
+            }
+        } else {
+            System.out.println("Nyalakan dulu donk HP-nya!!");
+        }
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+}
